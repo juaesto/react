c340c8c33e5a7040a67f9df53e8a8eb6db4bd17d
juanesparza <juanesparza@iti.es>
2021-12-09 17:17:59 +0100

    Array

---
+++ array/src/components/RecetaPizza.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class RecetaPizza extends React.Component {
+
+    render() {
+        const receta = {
+            nombre: 'Pizza',
+            ingredientes: ['Tomate', 'Queso', 'Nata'],
+            calorias: 400
+        };
+
+        const recetaPizzaContenedor = {
+            marginLeft: '60px'
+        }
+
+        return (
+            <div>
+                <h1>Receta: {receta.nombre}</h1>
+                <h2>Calorías: {receta.calorias}</h2>
+                <ol style={recetaPizzaContenedor}>
+                    {
+                        receta.ingredientes.map((ingrediente) => {
+                            return (
+                                <li style={{ width: '110px', textAlign: 'start' }} >
+                                    {ingrediente}
+                                </li>
+                            )
+                        })
+                    }
+                </ol>
+            </div>
+        )
+    }
+}
