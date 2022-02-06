d6c7dae0dce6d5403f6acc00c6b8d6d98f6e5391
juanesparza <juanesparza@iti.es>
2022-02-06 19:36:54 +0100

    Redux

---
+++ redux/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { startAction } from './actions/startAction';
+import { stopAction } from "./actions/stopAction";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            className={
+              "App-logo" +
+              (this.props.rotating ? "" : " App-logo-paused")
+            }
+            alt="logo"
+            onClick={
+              this.props.rotating ?
+                this.props.stopAction : this.props.startAction
+            }
+            style={{ "pointer-events": "all" }}
+          />
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
