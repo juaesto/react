dd2ec04f20cfff68fd5984e5e8c01ae6260c744c
juanesparza <juanesparza@iti.es>
2022-05-23 17:18:15 +0200

    ejercicios

--- ejercicios/ej4/src/components/Incremento.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export default function Incremento() {
-    const [incremento, setIncremento] = useState(0);
-
-    return (
-        <button onClick={() => setIncremento(incremento + 1)}>{incremento}</button>
-    );
-}
